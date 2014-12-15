9ae4d09cd9b34fe2115e0c2e138b950945616762
dancii <gromati163@gmail.com>
2014-12-15 21:23:46 +0100

    Some fixes

--- src/com/example/lab3a_v1/MainActivity.java
+++ src/com/example/lab3a_v1/MainActivity.java
@@ -1,42 +1,21 @@
 package com.example.lab3a_v1;
 
-import org.w3c.dom.Text;
-
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.ActivityInfo;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.graphics.drawable.AnimationDrawable;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.ImageView.ScaleType;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements SensorEventListener{
 
-	CustomDrawableView mCustomDrawableView = null;
-    ShapeDrawable mDrawable = new ShapeDrawable();
     public static int x;
     public static int y;
     public static int z;
@@ -51,6 +30,7 @@ public class MainActivity extends Activity implements SensorEventListener{
     float lastX = 0;
     float lastY = 0;
     float lastZ = 0;
+    private ImageView plantImage = null;
     
 
     private SensorManager sensorManager = null;
@@ -59,20 +39,18 @@ public class MainActivity extends Activity implements SensorEventListener{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		 Log.d("Lab3", "Test1"); 
+		Log.d("Lab3", "Test1"); 
 		
+		plantImage = (ImageView) findViewById(R.id.imgAnimation);
+		 
 		shakeIndex=0;
 		shakeArrayX= new int[10];
 		shakeArrayY= new int[10];
 		shakeArrayZ= new int[10];
 		
-		//image.setImageResource(R.drawable.android1);
-		//
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		mCustomDrawableView = new CustomDrawableView(this);
-		setContentView(mCustomDrawableView);
 	}
 
 	@Override
@@ -90,7 +68,6 @@ public class MainActivity extends Activity implements SensorEventListener{
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			isShake=false;
-			setContentView(mCustomDrawableView);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -118,7 +95,7 @@ public class MainActivity extends Activity implements SensorEventListener{
 	               // y=2*100;
 	                y = (int)sensorEvent.values[1]*5; 
 	                z = (int) sensorEvent.values[2]*5;
-	                
+	                changePicture(x);
 	                Log.d("Lab3", "X value: "+x); 
 	                
 	                gravity[0] = alpha * gravity[0] +(1-alpha) * (sensorEvent.values[0]);
@@ -156,13 +133,10 @@ public class MainActivity extends Activity implements SensorEventListener{
 	                	
 	                	if((medelX/10)>sens){
 	                		isShake = true;
-	                		animationLeaf();
 	                	}else if((medelY/10)>sens){
 	                		isShake = true;
-	                		animationLeaf();
 	                	}else if((medelZ/10)>sens){
 	                		isShake = true;
-	                		animationLeaf();
 	                	}
 	                		/*
 	                	if(speed > 250){
@@ -185,15 +159,6 @@ public class MainActivity extends Activity implements SensorEventListener{
 	        }
 	}
 	
-	public void animationLeaf(){
-		setContentView(R.layout.activity_main);
-		ImageView leafImageView = (ImageView) findViewById(R.id.imgAnimation);
-		leafImageView.setBackgroundResource(R.drawable.leaffall);
-		AnimationDrawable leafAnimation = (AnimationDrawable) leafImageView.getBackground();
-		
-		leafAnimation.start();
-	}
-	
 	@Override
     protected void onResume()
     {
@@ -214,181 +179,47 @@ public class MainActivity extends Activity implements SensorEventListener{
 	        super.onStop();
 	    }
 	 
-	 public class CustomDrawableView extends View {
-			static final int width = 50;
-		    static final int height = 50;
-		    int x, y;
-		    private Drawable flowerCenter,flowerRight1,flowerRight2, flowerRight3, flowerRight4, flowerRight5, flowerRight6, flowerRight7;
-		    private Drawable flowerLeft1,flowerLeft2, flowerLeft3, flowerLeft4, flowerLeft5, flowerLeft6, flowerLeft7, flowerdead;
-			
-		        public CustomDrawableView(Context context){
-		            super(context);
-		            Resources resource = context.getResources();
-		            flowerCenter = (Drawable) resource.getDrawable(R.drawable.flowercenter);
-		            flowerRight1 = (Drawable) resource.getDrawable(R.drawable.flowerright1);
-		            flowerRight2 = (Drawable) resource.getDrawable(R.drawable.flowerright2);
-		            flowerRight3 = (Drawable) resource.getDrawable(R.drawable.flowerright3);
-		            flowerRight4 = (Drawable) resource.getDrawable(R.drawable.flowerright4);
-		            flowerRight5 = (Drawable) resource.getDrawable(R.drawable.flowerright5);
-		            flowerRight6 = (Drawable) resource.getDrawable(R.drawable.flowerright6);
-		            flowerRight7 = (Drawable) resource.getDrawable(R.drawable.flowerright7);
-		            flowerdead = (Drawable) resource.getDrawable(R.drawable.flowerdead);
-		            flowerLeft1 = (Drawable) resource.getDrawable(R.drawable.flowerleft1);
-		            flowerLeft2 = (Drawable) resource.getDrawable(R.drawable.flowerleft2);
-		            flowerLeft3 = (Drawable) resource.getDrawable(R.drawable.flowerleft3);
-		            flowerLeft4 = (Drawable) resource.getDrawable(R.drawable.flowerleft4);
-		            flowerLeft5 = (Drawable) resource.getDrawable(R.drawable.flowerleft5);
-		            flowerLeft6 = (Drawable) resource.getDrawable(R.drawable.flowerleft6);
-		            flowerLeft7 = (Drawable) resource.getDrawable(R.drawable.flowerleft7);
-		            
-		        }
-
-		        protected void onDraw(Canvas canvas){
-		        	
-		        	x = 30;
-		            y = 30;
-		        	
-		        	/*if(shakeIndex>9)
-		        		shakeIndex=0;
-		        	
-		        	shakeArrayX[shakeIndex] = (int) accel[0];
-		        	shakeArrayY[shakeIndex] = (int) accel[1];
-		        	shakeArrayZ[shakeIndex] = (int) accel[2];
-		        	
-		        	
-		        		
-		        	
-		        	if(shakeArrayX[shakeIndex]-shakeArrayX[(shakeIndex+1)%10]>95){  
-		        		shaker=true;
-		        	}else if(shakeArrayY[shakeIndex]-shakeArrayY[(shakeIndex+1)%10]>95){
-		        		shaker=true;
-		        	}else if(shakeArrayZ[shakeIndex]-shakeArrayZ[(shakeIndex+1)%10]>95){
-		        		shaker=true;
-		        	}
-		        	
-		        	
-		        	
-		        	else if(shaker){
-		        		int iw = flowerDead.getIntrinsicWidth();
-		        		int ih = flowerDead.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerDead.setBounds(bounds);
-		        		flowerDead.draw(canvas);
-		        	}*/
-		        	
-		        	if(isShake){
-		        		/*int iw = flowerdead.getIntrinsicWidth();
-		        		int ih = flowerdead.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerdead.setBounds(bounds);
-		        		flowerdead.draw(canvas);*/
-		        		
-		        	}else if(MainActivity.x >=0 && MainActivity.x < 12){
-		        		int iw = flowerCenter.getIntrinsicWidth();
-		        		int ih = flowerCenter.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerCenter.setBounds(bounds);
-		        		flowerCenter.draw(canvas);
-		        	}else if(MainActivity.x >=12 && MainActivity.x < 24){
-		        		int iw = flowerRight1.getIntrinsicWidth();
-		        		int ih = flowerRight1.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight1.setBounds(bounds);
-		        		flowerRight1.draw(canvas);
-		        	}else if(MainActivity.x >=24 && MainActivity.x < 36){
-		        		int iw = flowerRight2.getIntrinsicWidth();
-		        		int ih = flowerRight2.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight2.setBounds(bounds);
-		        		flowerRight2.draw(canvas);
-		        	}else if(MainActivity.x >=36 && MainActivity.x < 48){
-		        		int iw = flowerRight3.getIntrinsicWidth();
-		        		int ih = flowerRight3.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight3.setBounds(bounds);
-		        		flowerRight3.draw(canvas);
-		        	}else if(MainActivity.x >=48 && MainActivity.x < 60){
-		        		int iw = flowerRight4.getIntrinsicWidth();
-		        		int ih = flowerRight4.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight4.setBounds(bounds);
-		        		flowerRight4.draw(canvas);
-		        	}else if(MainActivity.x >=60 && MainActivity.x < 72){
-		        		int iw = flowerRight5.getIntrinsicWidth();
-		        		int ih = flowerRight5.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight5.setBounds(bounds);
-		        		flowerRight5.draw(canvas);
-		        	}else if(MainActivity.x >=72 && MainActivity.x < 84){
-		        		int iw = flowerRight6.getIntrinsicWidth();
-		        		int ih = flowerRight6.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight6.setBounds(bounds);
-		        		flowerRight6.draw(canvas);
-		        	}else if(MainActivity.x >=84){
-		        		int iw = flowerRight7.getIntrinsicWidth();
-		        		int ih = flowerRight7.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerRight7.setBounds(bounds);
-		        		flowerRight7.draw(canvas);
-		        	}else if(MainActivity.x <=0 && MainActivity.x > -12){
-		        		int iw = flowerCenter.getIntrinsicWidth();
-		        		int ih = flowerCenter.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerCenter.setBounds(bounds);
-		        		flowerCenter.draw(canvas);
-		        	}else if(MainActivity.x <=-12 && MainActivity.x > -24){
-		        		int iw = flowerLeft1.getIntrinsicWidth();
-		        		int ih = flowerLeft1.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft1.setBounds(bounds);
-		        		flowerLeft1.draw(canvas);
-		        	}else if(MainActivity.x <=-24 && MainActivity.x > -36){
-		        		int iw = flowerLeft2.getIntrinsicWidth();
-		        		int ih = flowerLeft2.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft2.setBounds(bounds);
-		        		flowerLeft2.draw(canvas);
-		        	}else if(MainActivity.x <=-36 && MainActivity.x > -48){
-		        		int iw = flowerLeft3.getIntrinsicWidth();
-		        		int ih = flowerLeft3.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft3.setBounds(bounds);
-		        		flowerLeft3.draw(canvas);
-		        	}else if(MainActivity.x <=-48 && MainActivity.x > -60){
-		        		int iw = flowerLeft4.getIntrinsicWidth();
-		        		int ih = flowerLeft4.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft4.setBounds(bounds);
-		        		flowerLeft4.draw(canvas);
-		        	}else if(MainActivity.x <=-60 && MainActivity.x > -72){
-		        		int iw = flowerLeft5.getIntrinsicWidth();
-		        		int ih = flowerLeft5.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft5.setBounds(bounds);
-		        		flowerLeft5.draw(canvas);
-		        	}else if(MainActivity.x <=-72 && MainActivity.x > -84){
-		        		int iw = flowerLeft6.getIntrinsicWidth();
-		        		int ih = flowerLeft6.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft6.setBounds(bounds);
-		        		flowerLeft6.draw(canvas);
-		        	}else if(MainActivity.x <=-84){
-		        		int iw = flowerLeft7.getIntrinsicWidth();
-		        		int ih = flowerLeft7.getIntrinsicHeight();
-		        		Rect bounds = new Rect(x,y,x+iw, y+ih);
-		        		flowerLeft7.setBounds(bounds);
-		        		flowerLeft7.draw(canvas);
-		        	}
-		        	
-		        	 
-		        	shakeIndex++;
-		            invalidate();
-		            
-		        }
-			
-
-		}
-	 
+	 public void changePicture(int xPos){
+		 if(isShake){
+			 plantImage.setBackgroundResource(R.drawable.leaffall);
+			 AnimationDrawable leafAnimation = (AnimationDrawable) plantImage.getBackground();
+			 leafAnimation.start();
+		 }else{
+			 if(xPos >=0 && xPos< 12){
+				 plantImage.setBackgroundResource(R.drawable.flowercenter);
+			 }else if(xPos >=12 && xPos < 24){
+				 plantImage.setBackgroundResource(R.drawable.flowerright1);
+			 }else if(xPos >=24 && xPos < 36){
+				 plantImage.setBackgroundResource(R.drawable.flowerright2);
+			 }else if(xPos >=36 && xPos< 48){
+				 plantImage.setBackgroundResource(R.drawable.flowerright3);
+			 }else if(xPos >=48 && xPos < 60){
+				 plantImage.setBackgroundResource(R.drawable.flowerright4);
+			 }else if(xPos >=60 && xPos < 72){
+				 plantImage.setBackgroundResource(R.drawable.flowerright5);
+			 }else if(xPos >=72 && xPos < 84){
+				 plantImage.setBackgroundResource(R.drawable.flowerright6);
+			 }else if(xPos >=84){
+				 plantImage.setBackgroundResource(R.drawable.flowerright7);
+			 }else if(xPos <=0 && xPos > -12){
+				 plantImage.setBackgroundResource(R.drawable.flowercenter);
+			 }else if(xPos <=-12 && xPos > -24){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft1);
+			 }else if(xPos <=-24 && xPos > -36){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft2);
+			 }else if(xPos <=-36 && xPos > -48){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft3);
+			 }else if(xPos <=-48 && xPos > -60){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft4);
+			 }else if(xPos <=-60 && xPos > -72){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft5);
+			 }else if(xPos <=-72 && xPos > -84){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft6);
+			 }else if(xPos <=-84){
+				 plantImage.setBackgroundResource(R.drawable.flowerleft7);
+			 }
+		 }
+		 
+	 }
 }
 
